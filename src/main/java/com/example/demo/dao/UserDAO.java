3f2e5e8316184bf2886f130089d47facdda34755
Piotrek <drabina2012@gmail.com>
2021-04-18 16:27:45 +0000

    adding, editing and deleting users

--- src/main/java/com/example/demo/dao/UserDAO.java
+++ src/main/java/com/example/demo/dao/UserDAO.java
@@ -3,4 +3,11 @@ import java.util.List;
 import com.example.demo.model.User;
 
 public interface UserDAO {
+
+    public void addUser(User user);
+    public void updateUser(User user);
+    public User getUser(String id);
+    public void deleteUser(String id);
+    public List<User> getUsers();
+
 }
