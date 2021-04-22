b62380d964212d9c2f1187af75deddc738a6f89c
Bartosz Wyciszkiewicz <bartosz.wyciszkiewicz@gmail.com>
2021-04-22 20:15:53 +0000

    login and authentication

---
+++ src/main/java/com/bezkoder/spring/security/postgresql/controllers/ViewController.java
@@ -0,0 +1,43 @@
+package com.bezkoder.spring.security.postgresql.controllers;
+
+import com.bezkoder.spring.security.postgresql.payload.request.LoginRequest;
+import com.bezkoder.spring.security.postgresql.payload.request.SignupRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+@RestController
+//@Controller
+@RequestMapping("/api/auth")
+public class ViewController {
+
+
+    //    @GetMapping("login")
+//    public ModelAndView getWelcomePageAsModel() {
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.setViewName("test");
+//        return modelAndView;
+//    }
+    @GetMapping("/login")
+    public ModelAndView getWelcomePageAsModel(Model model) {
+        model.addAttribute("loginRequest",new LoginRequest());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login");
+        return modelAndView;
+    }
+    @GetMapping("/signup")
+    public ModelAndView getSignUpView(Model model) {
+        model.addAttribute("signupRequest",new SignupRequest());
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("signup");
+        return modelAndView;
+    }
+
+
+//    @PostMapping("catch")
+//    public String postBody(@RequestBody String fullname)
+//    {
+//        return "Hello" + fullname;
+//    }
+
+}
