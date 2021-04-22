555585c6c5768af7eb5784a74ea0d39fd8f5592c
starzykbartlomiej <starzyk.bartlomiej@gmail.com>
2021-04-22 22:36:33 +0000

    test

--- src/main/java/com/bezkoder/spring/security/postgresql/controllers/UserController.java
+++ src/main/java/com/bezkoder/spring/security/postgresql/controllers/UserController.java
@@ -1,10 +1,5 @@
 package com.bezkoder.spring.security.postgresql.controllers;
 
-import java.util.List;
-
-import com.bezkoder.spring.security.postgresql.models.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
