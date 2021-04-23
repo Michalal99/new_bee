601de58c2f1ef0a7a6b6562d11fc97f6752c08a3
starzykbartlomiej <starzyk.bartlomiej@gmail.com>
2021-04-23 11:15:24 +0000

    nie dziala css

--- src/main/java/com/bee/controller/HomeController.java
+++ src/main/java/com/bee/controller/HomeController.java
@@ -3,13 +3,12 @@ package com.bee.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String hello(Model model) {
+    public String home(Model model) {
         model.addAttribute("message", "Welcome to home page");
         return "home";
     }
