68a7776b787aa88c2154a46057dee8cc2b8479da
starzykbartlomiej <starzyk.bartlomiej@gmail.com>
2021-04-24 00:41:44 +0000

    CRUD team working

--- src/main/java/com/bee/controller/TeamController.java
+++ src/main/java/com/bee/controller/TeamController.java
@@ -3,42 +3,59 @@ package com.bee.controller;
 import com.bee.models.Team;
 import com.bee.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Controller
 @RequestMapping("/teams")
 public class TeamController {
-    private final TeamService teamService;
-
     @Autowired
-    public TeamController(TeamService teamService) {
-        this.teamService = teamService;
+    private TeamService teamService;
+
+    @GetMapping("/create")
+    public String createTeam(Model model) {
+        Team team = new Team();
+        model.addAttribute("team", team);
+        return "Team/create";
     }
 
     @PostMapping
-    public ResponseEntity<Team> addEmployee(@RequestBody Team team) {
-        Team newTeam = teamService.addTeam(team);
-        return new ResponseEntity<>(newTeam, HttpStatus.CREATED);
+    public RedirectView storeTeam(@ModelAttribute("team") Team team, Model model) {
+        teamService.addTeam(team);
+        return new RedirectView("/teams");
     }
 
     @GetMapping
-    public String getAllEmployees (Model model) {
+    public String indexTeams(Model model) {
         List<Team> teams = teamService.findAllTeams();
         model.addAttribute("teams", teams);
         return "Team/index";
     }
 
     @GetMapping("/{id}")
-    public String getEmployeeById (@PathVariable("id") Long id, Model model) {
+    public String showTeam(@PathVariable("id") Long id, Model model) {
         Team newTeam = teamService.findTeamById(id);
         model.addAttribute("team", newTeam);
         return "Team/show";
+    }
+
+    @GetMapping("/delete/{id}")
+    public RedirectView deleteTeam(@PathVariable("id") Long id) {
+        Team oldTeam = teamService.findTeamById(id);
+        teamService.deleteTeam(oldTeam);
+        return new RedirectView("/teams");
+    }
+
+    @GetMapping("/edit/{id}")
+    public String editTeam(@PathVariable("id") Long id, Model model) {
+        Team team = teamService.findTeamById(id);
+        model.addAttribute("team", team);
+        return "Team/edit";
     }
 
 }
--- src/main/java/com/bee/service/TeamService.java
+++ src/main/java/com/bee/service/TeamService.java
@@ -3,29 +3,30 @@ package com.bee.service;
 import com.bee.models.Team;
 import com.bee.repository.TeamRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class TeamService {
-    private final TeamRepo teamRepo;
-
     @Autowired
-    public TeamService(TeamRepo teamRepo) {
-        this.teamRepo = teamRepo;
-    }
+    private TeamRepo teamRepo;
 
-    public Team addTeam(Team team) {
-        return teamRepo.save(team);
+    public void addTeam(Team team) {
+        teamRepo.save(team);
     }
 
     public List<Team> findAllTeams() {
-        return teamRepo.findAll();
+        return teamRepo.findAll(Sort.by(Sort.Direction.ASC, "id"));
     }
 
     public Team findTeamById(Long id){
         //TODO obsluga wyjatkow
         return teamRepo.findTeamById(id).orElseThrow();
+    }
+
+    public void deleteTeam(Team team){
+        teamRepo.delete(team);
     }
 }
