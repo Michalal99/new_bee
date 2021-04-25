a6bae91a357407c6a712975cfa97ea1e3b670ca4
Bartosz Wyciszkiewicz <bartosz.wyciszkiewicz@gmail.com>
2021-04-25 10:16:19 +0000

    registration

--- src/main/java/com/bezkoder/spring/security/postgresql/controllers/AuthController.java
+++ src/main/java/com/bezkoder/spring/security/postgresql/controllers/AuthController.java
@@ -1,27 +1,33 @@
 package com.bezkoder.spring.security.postgresql.controllers;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.net.http.HttpResponse;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Produces;
 
+import antlr.StringUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.HttpMessageReader;
+import org.springframework.http.codec.multipart.MultipartHttpMessageReader;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.bezkoder.spring.security.postgresql.models.ERole;
 import com.bezkoder.spring.security.postgresql.models.Role;
@@ -34,10 +40,12 @@ import com.bezkoder.spring.security.postgresql.repository.RoleRepository;
 import com.bezkoder.spring.security.postgresql.repository.UserRepository;
 import com.bezkoder.spring.security.postgresql.security.jwt.JwtUtils;
 import com.bezkoder.spring.security.postgresql.security.services.UserDetailsImpl;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
+
 public class AuthController {
 	@Autowired
 	AuthenticationManager authenticationManager;
@@ -53,6 +61,9 @@ public class AuthController {
 
 	@Autowired
 	JwtUtils jwtUtils;
+
+
+	//public ResponseEntity<?> logout ()
 
 	@PostMapping("/signin")
 	public ResponseEntity<?> authenticateUser(@Valid @RequestBody String httpResponse)
@@ -105,26 +116,77 @@ public class AuthController {
 //												 userDetails.getEmail(),
 //												 roles));
 //	}
-
+//@POST
+//@Consumes("multipart/form-data")
+//@PostMapping("/signup")
+//public void put(@RequestBody  Map<String, String> customers)
+//{
+//	var x = customers;
+//	System.out.println("dupa");
+//}
+//public String echo( @RequestBody String requestBody) throws Exception {
+//		var x = requestBody;
+//	return requestBody;
+//}
 	@PostMapping("/signup")
-	public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest) {
+//	@RequestMapping(path = "/signup", method = RequestMethod.POST,
+//			consumes = {"multipart/form-data"})
+	public ResponseEntity<?> registerUser(@Valid @RequestBody String response) {
+//		public ResponseEntity<?> registerUser(@RequestBody  Object signUpRequest) {
+//
+//
+String a = response;
+Map<String,String> args = new HashMap<>();
+
+		while(a.contains("&"))
+{
+
+		String key = a.substring(0, a.indexOf("="));
+		String val = a.substring(a.indexOf("=") + 1, a.indexOf("&"));
+		args.put(key, val);
+
+		a = a.substring(a.indexOf("&") + 1, a.length() - 1);
+
+}
+		var email = args.get("email").replace("%40","@");
+		args.replace("email",email);
+	//	args.replace("email",args.get("mail").replace("%40","@"));
+		SignupRequest signUpRequest = new SignupRequest();
+		signUpRequest.setUsername(args.get("username"));
+
+		if(args.get("password").compareTo(args.get("password_confirm"))!=0)
+		{
+			return ResponseEntity
+					.badRequest()
+					.body(new MessageResponse("Error: Password must be the same!"));
+		}
+
+		signUpRequest.setPassword(args.get("password"));
+		signUpRequest.setEmail(args.get("email"));
+		Set<String> role = new HashSet<>() ;
 
 
 
-		//var x = signUpRequest;
+		if(args.containsKey("user"))
+			role.add("user");
+		if(args.containsKey("mod"))
+			role.add("mod");
+		if(args.containsKey("admin"))
+			role.add("admin");
 
+		signUpRequest.setRole(role);
 		if (userRepository.existsByUsername(signUpRequest.getUsername())) {
 			return ResponseEntity
 					.badRequest()
 					.body(new MessageResponse("Error: Username is already taken!"));
 		}
-
+//
 		if (userRepository.existsByEmail(signUpRequest.getEmail())) {
 			return ResponseEntity
 					.badRequest()
 					.body(new MessageResponse("Error: Email is already in use!"));
 		}
-
+//
 		// Create new user's account
 		User user = new User(signUpRequest.getUsername(),
 							 signUpRequest.getEmail(),
@@ -138,8 +200,9 @@ public class AuthController {
 					.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
 			roles.add(userRole);
 		} else {
-			strRoles.forEach(role -> {
-				switch (role) {
+
+			strRoles.forEach(rol -> {
+				switch (rol) {
 				case "admin":
 					Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
 							.orElseThrow(() -> new RuntimeException("Error: Role is not found."));
@@ -163,6 +226,6 @@ public class AuthController {
 		user.setRoles(roles);
 		userRepository.save(user);
 
-		return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+	return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
 	}
 }
