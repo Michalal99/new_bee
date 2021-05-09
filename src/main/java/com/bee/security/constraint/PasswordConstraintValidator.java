54dc93257698800f6221018d20c4e0812d5c36cb
starzykbartlomiej <starzyk.bartlomiej@gmail.com>
2021-05-09 21:15:10 +0000

    password constraint

--- src/main/java/com/bee/security/constraint/PasswordConstraintValidator.java
+++ src/main/java/com/bee/security/constraint/PasswordConstraintValidator.java
@@ -1,18 +1,14 @@
 package com.bee.security.constraint;
-
 import org.passay.*;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
     @Override
     public void initialize(ValidPassword arg0) {
     }
-
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
         PasswordValidator validator = new PasswordValidator(Arrays.asList(
@@ -22,7 +18,6 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
                 new CharacterRule(EnglishCharacterData.Digit, 1),
                 new CharacterRule(EnglishCharacterData.Special, 1),
                 new WhitespaceRule()
-
         ));
         RuleResult result = validator.validate(new PasswordData(password));
         if (result.isValid()) {
@@ -31,10 +26,8 @@ public class PasswordConstraintValidator implements ConstraintValidator<ValidPas
 //        buildConstraintViolation(context, validator, result);
         return false;
     }
-
     public void buildConstraintViolation(ConstraintValidatorContext context, PasswordValidator validator, RuleResult result) {
         List<String> messages = validator.getMessages(result);
-
         String messageTemplate = messages.stream()
                 .collect(Collectors.joining(","));
         context.buildConstraintViolationWithTemplate(messageTemplate)
