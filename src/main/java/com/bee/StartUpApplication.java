5e7100f2dbfc169de94b251709e7301ecb2fc610
starzykbartlomiej <starzyk.bartlomiej@gmail.com>
2021-05-31 12:43:47 +0000

    another one

--- src/main/java/com/bee/StartUpApplication.java
+++ src/main/java/com/bee/StartUpApplication.java
@@ -21,26 +21,26 @@ public class  StartUpApplication {
 
 }
 
-//@Component
-//class DemoCommandLineRunner implements CommandLineRunner {
-//
-//    @Autowired
-//    private RoleRepository roleRepository;
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//          roleRepository.deleteAll();
-//        var x1= new Role();
-//        x1.setName(ERole.ROLE_ADMIN);
-//        roleRepository.save(x1);
-//
-//        var x2= new Role();
-//        x2.setName(ERole.ROLE_USER);
-//        roleRepository.save(x2);
-//
-//        var x3= new Role();
-//        x3.setName(ERole.ROLE_MODERATOR);
-//        roleRepository.save(x3);
-//    }
-//}
+@Component
+class DemoCommandLineRunner implements CommandLineRunner {
+
+    @Autowired
+    private RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+          roleRepository.deleteAll();
+        var x1= new Role();
+        x1.setName(ERole.ROLE_ADMIN);
+        roleRepository.save(x1);
+
+        var x2= new Role();
+        x2.setName(ERole.ROLE_USER);
+        roleRepository.save(x2);
+
+        var x3= new Role();
+        x3.setName(ERole.ROLE_MODERATOR);
+        roleRepository.save(x3);
+    }
+}
